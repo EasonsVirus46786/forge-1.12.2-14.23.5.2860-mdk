6153d70e0f0f3df6c1161e67dafb237cba8089d2
unknown <Virus46786@163.com>
2022-08-02 17:07:46 +0800

    Initial commit

---
+++ src/main/java/com/virus/testmod/TestMod.java
@@ -0,0 +1,49 @@
+package com.virus.testmod;
+
+import com.virus.testmod.item.ModItems;
+import com.virus.testmod.proxy.CommonProxy;
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod(modid = TestMod.modId, name = TestMod.name, version = TestMod.version)
+public class TestMod {
+
+    public static final String modId = "testmod";
+    public static final String name = "Test Mod";
+    public static final String version = "1.0.0";
+
+    @Mod.Instance(modId)
+    public static TestMod instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        System.out.println(name + " is loading!");
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+
+    @SidedProxy(serverSide = "com.virus.testmod.proxy.CommonProxy", clientSide = "com.virus.testmod.proxy.ClientProxy")
+    public static CommonProxy proxy;
+
+    @Mod.EventBusSubscriber
+    public static class RegistrationHandler {
+        @SubscribeEvent
+        public static void registerItems(RegistryEvent.Register<Item> event) {
+            ModItems.register(event.getRegistry());
+        }
+    }
+}
